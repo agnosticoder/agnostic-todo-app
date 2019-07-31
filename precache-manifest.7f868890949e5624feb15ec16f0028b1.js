92bc7848b5e90e2c7420b5f775fa26f86e2d1964
agnosticoder <agnosticoder@gmail.com>
2019-07-31 01:32:59 -0400

    Updates

---
+++ precache-manifest.7f868890949e5624feb15ec16f0028b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10f55687f92cc774bb5618c368dbeae",
+    "url": "/agnostic-todo-app/index.html"
+  },
+  {
+    "revision": "cb2fc510abc187f83fc9",
+    "url": "/agnostic-todo-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f186f58976874cf81238",
+    "url": "/agnostic-todo-app/static/css/main.70e69bc0.chunk.css"
+  },
+  {
+    "revision": "cb2fc510abc187f83fc9",
+    "url": "/agnostic-todo-app/static/js/2.1f293c58.chunk.js"
+  },
+  {
+    "revision": "f186f58976874cf81238",
+    "url": "/agnostic-todo-app/static/js/main.8bbb0ee3.chunk.js"
+  },
+  {
+    "revision": "04fea94da6adaaf0cba0",
+    "url": "/agnostic-todo-app/static/js/runtime~main.790e3751.js"
+  }
+]);
