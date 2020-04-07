f9d7ae2af700832fc4e4b1908ec3c29700585000
agnosticoder <agnosticoder@gmail.com>
2020-04-07 13:04:33 -0400

    Updates

---
+++ precache-manifest.23f69ac4eb8dcc9c0e642dfd8bd7708b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f805da36e8489512a190731b3226f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e507bfe6420e93e4e06",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f5bc40d7527c0690a5af",
+    "url": "/static/css/main.1757f233.chunk.css"
+  },
+  {
+    "revision": "1e507bfe6420e93e4e06",
+    "url": "/static/js/2.009faa43.chunk.js"
+  },
+  {
+    "revision": "f5bc40d7527c0690a5af",
+    "url": "/static/js/main.0c1127cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
